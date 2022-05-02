9d3e5afacb3144e1d2b712596b8c849ff73ed1c5
Charly Aguirre <charly.aguirre@pacific-hs.com>
2022-05-02 07:40:43 -0500

    colors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 const colors = require('colors');
 
@@ -12,4 +13,18 @@ const server = http.createServer(handleServer);
 server.listen(3000, function () {
     console.log('Server on port 3000'.green);
 })
+*/
+
+const express = require('express');
+const colors = require('colors');
+const server = express();
+
+server.get('/', function (req, res) {
+    res.send('<h1>Hola Mundo express nodejs</h1>');
+    res.end();
+});
+
+server.listen(3000, function () {
+    console.log('Server on port 3000'.red);
+});
 
